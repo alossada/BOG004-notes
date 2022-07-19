ca795824201396dbaa72652476fbd1ca23ec3d47
anagarcia791 <amgt2697@gmail.com>
2022-07-19 14:22:14 -0500

    initiaÃl settings

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+
+          XOXO
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
