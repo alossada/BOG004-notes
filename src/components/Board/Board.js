e84eaba428f8a5443632c525388faf5b9bed909c
Anamaria MC <anymedinc14@gmail.com>
2022-07-22 16:15:25 -0500

    se importa useAuth para prueba de traer la información del usuario logueado

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,1 +1,16 @@
 // vista board
+import { useAuth } from "../../context/authContext"
+
+export default function Board() { 
+
+  const {user} =useAuth()//me trae la información del usuario (verifiga el login)
+  console.log(user);
+
+  return (
+    <div>
+      <h1>
+      Aca estaran las notas de {user.email}
+      </h1>
+    </div>   
+  )
+}
